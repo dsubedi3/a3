899eb54c9d811c7997fda767e2fa2d2a7b367f90
Dipsan Subedi <dipsonsubedi68@gmail.com>
2025-02-20 14:35:11 -0500

    i make some change

---
+++ views/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [  
+    "./views/**/*.html",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['cupcake'],
+  },
+}
+
